6713042d75a3b1418e4aa518453cd4239e51e4f6
kevin.lee@refreshlabs.co <kevin.lee@lynkr.xyz>
2017-12-21 20:42:58 -0800

    commenting

--- functions/index.js
+++ functions/index.js
@@ -3,16 +3,21 @@ const DialogflowApp = require('actions-on-google').DialogflowApp;
 const functions = require('firebase-functions');
 
 exports.gdax = functions.https.onRequest((request, response) => {
+
+    // Initialize DialogflowApp object
     const app = new DialogflowApp({request, response});
 
+    // Intents
     const WELCOME_INTENT = 'input.welcome';
     const GDAX_INTENT = 'input.gdax';
 
+    // Parameters
     const QUANTITY = 'quantity';
     const PRICE = 'price';
     const CRYPTO = 'crypto';
     const PURCHASE = 'purchase';
 
+    // Functions to handle requests
     function welcomeIntent(app) {
         app.ask('Welcome to GDAX trader.');
     }
@@ -25,10 +30,12 @@ exports.gdax = functions.https.onRequest((request, response) => {
         app.ask("Creating order to " + purchase + " " + quantity + " " + crypto + " at " + price);
     }
 
+    // Build action map
     let actionMap = new Map();
     actionMap.set(GDAX_INTENT, gdaxIntent);
     actionMap.set(WELCOME_INTENT, welcomeIntent);
     
+    // Initialize action map
     app.handleRequest(actionMap);
 
 });
